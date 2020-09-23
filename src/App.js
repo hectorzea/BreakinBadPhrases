766582b68b5751f24966544bb7825280c4edc26f
Hector <hzea@inclusionservices.com>
2020-09-23 14:20:28 -0300

    last

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from "@emotion/styled";
+import Phrase from "./components/Phrase";
+
+const Container = styled.div`
+display:flex;
+align-items: center;
+padding-top:5rem;
+flex-direction: column;
+`;
+
+const Button = styled.button`
+background:-webkit-linear-gradient(top left, #007d35 0% ,#007d35 40%, #0f574e 100%);
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color:#fff;
+margin-top: 3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: solid 2px black;
+transition:background-size .8s ease;
+
+:hover{
+cursor: pointer;
+background-size: 400px;
+}
+
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //state of phrases
+
+    const [phrase, setPhrase] = useState({});
+
+    const checkAPI = async () => {
+        const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+        const phrase = await api.json();
+        console.log(phrase[0]);
+        setPhrase(phrase[0])
+    };
+
+    useEffect(() => {
+        checkAPI();
+    }, []);
+
+    return (
+        <Container>
+            <Phrase phrase={phrase}/>
+            <Button onClick={checkAPI}>
+                Get Phrase
+            </Button>
+        </Container>
+    );
 }
 
 export default App;
